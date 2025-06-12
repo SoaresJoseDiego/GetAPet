26f4dbad3fa17007a37ffe4b630f949e00e5d626
Diego Soares <dsadamovich@gmail.com>
2025-06-12 17:42:49 -0300

    refatora o componente PetDetails para melhorar a legibilidade e a estrutura do código

--- frontend/src/components/pages/Pet/PetDetails.js
+++ frontend/src/components/pages/Pet/PetDetails.js
@@ -1,15 +1,26 @@
-import styles from './PetDetails.module.css';
+import styles from "./PetDetails.module.css";
 
-import api from '../../../utils/api';
-import { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import api from "../../../utils/api";
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 
-/* hooks*/ 
+/* hooks*/
 
-import useFlashMessage from '../../../hooks/useFlashMessage';
+import useFlashMessage from "../../../hooks/useFlashMessage";
 
-export default function PetDetails(){
-    return(
-        <h1>Página de Pet</h1>
-    )
+export default function PetDetails() {
+  const [pet, setPet] = useState({});
+  const { id } = useParams();
+  const { setFlashMessage } = useFlashMessage();
+  const [token] = useState(localStorage.getItem("token") || "");
+
+
+
+  useEffect(() => {
+    api.get(`/pets/${id}`).then((response) => {
+      setPet(response.data.pet);
+    });
+  }, [id]);
+
+  return <h1>{pet.name}</h1>;
 }
